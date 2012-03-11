79491639748d0cc52d0b979177d3da9cdb9d2a48
Mike Santo <mike@rsdcompany.com>
2012-03-11 13:50:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodbank::Application.config.secret_token = '8e627962c0ef81f9b986f1daa5f80b9996b7fd53a5dab29c3b3f03e257a151021fb123c944b79e8b82768eefbf0be5ae76c0392cd997d0cc90e702e51b560316'
