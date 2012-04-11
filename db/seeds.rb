d7e3205d2c85cf5243682d2f4c90a782f93a5254
Mike Santo <mike.j.santo@gmail.com>
2012-04-10 21:41:08 -0500

    flesh out seeds for dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,38 @@
 roles = Role.create([{name: 'Administrator'}, {name: 'User'}])
 
 organization_roles = OrganizationRole.create([{name: "Producer"}, {name: "Distributor"}])
+
+organizations = Organization.create([{
+																			name: "Food Bank", 
+																			state: "NE", 
+																			city: "Omaha", 
+																			street: "123 FoodBank Drive", 
+																			zip: "68121", 
+																			email: "person@foodbank.com",
+																			phone: 7654321,
+																			role_id: 2
+																		},
+																		{
+																			name: "Blackburn",
+																			state: "NE",
+																			city: "Omaha",
+																			street: "456 Blackburn Ave",
+																			zip: "68154",
+																			email: "person@blackburn.com",
+																			phone: 1234567,
+																			role_id: 1
+																		},
+																		{
+																			name: "Alternative Systems",
+																			state: "NE",
+																			city: "Omaha",
+																			street: "789 AltySys Road",
+																			zip: "68131",
+																			email: "mike.j.santo@gmail.com",
+																			phone: 5154191,
+																			role_id: 2
+																	}])
+
+me = User.create(fname: "Mike", lname: "Santo", password: 'asdf1234', 
+								password_confirmation: 'asdf1234', email: 'mike.j.santo@gmail.com',
+								phone: 4025154191, role_id: 1, organization_id: 3)
