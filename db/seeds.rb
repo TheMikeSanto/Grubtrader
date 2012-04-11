216da8376075cf854eec2c915adf0f60ae03150f
Mike Santo <mike.j.santo@gmail.com>
2012-04-10 21:05:58 -0500

    Added 2 roles and 2 organization roles in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+roles = Role.create({name: 'Administrator'},
+					{name: 'User'})
+
+organization_roles = OrganizationRole.create({name: "Producer"},
+											 {name: "Distributor"})
