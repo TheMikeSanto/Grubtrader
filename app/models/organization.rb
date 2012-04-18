922cd9355a6a8376954ec50935d8da8631dca356
Mike Santo <mike.j.santo@gmail.com>
2012-04-18 01:54:28 -0500

    organization validates presence of all of its fields

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,6 @@
 class Organization < ActiveRecord::Base
 	has_many :users
 	belongs_to :organization_role, foreign_key: :role_id
+
+	validates_presence_of :phone, :state, :city, :street, :zip, :email, :name, :role_id
 end
