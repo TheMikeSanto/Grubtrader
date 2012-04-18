d51c63c35a9366eb98366e392ed68a8bfb9ab652
Mike Santo <mike.j.santo@gmail.com>
2012-04-17 21:38:47 -0500

    moved message center into header div, also removed a bunch of demo js that we dont need

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+	if ($("#notifications").length > 0) {
+		console.log("word");
+		setTimeout(function() {
+			$("#notifications").fadeOut("slow");
+		}, 5000);
+	}
+})
