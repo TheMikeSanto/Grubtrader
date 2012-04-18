1b57b792ef94567526025e2c28409e9a7e25a385
Mike Santo <mike.j.santo@gmail.com>
2012-04-18 01:05:27 -0500

    removed extraneous organization rules on ability class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,8 +19,7 @@ class Ability
       can :manage, Report
       can :manage, Inventory
       can :view, Product
-      can :view, Organization
-      can [:create, :update], Organization, organization_id: user.organization_id
+      can [:update], Organization, organization_id: user.organization_id
     end
 
     if user.is_admin?
