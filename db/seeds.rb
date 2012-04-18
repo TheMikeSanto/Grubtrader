3f3da3b81aa08991aa9ccf710cad4dc0ad143281
Mike Santo <mike.j.santo@gmail.com>
2012-04-17 22:16:32 -0500

    more test users, organizations

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,8 +39,30 @@ organizations = Organization.create([{
 																			email: "mike.j.santo@gmail.com",
 																			phone: 5154191,
 																			role_id: 2
+																	},
+																	{
+																		name: "Local Growers", 
+																		state: "Nebraska", city: "Omaha", 
+																		street: "1234 Fake St.", zip: "68131", 
+																		phone: 4021234567, 
+																		email: "john@doe.com", 
+																		role_id: 3
 																	}])
 
 me = User.create(fname: "Mike", lname: "Santo", password: 'asdf1234', 
 								password_confirmation: 'asdf1234', email: 'mike.j.santo@gmail.com',
 								phone: 4025154191, role_id: 1, organization_id: 3)
+
+users = User.create([
+										{	fname: "John", lname: "Doe", password: "asdf1234", 
+											password_confirmation: 'asdf1234', email: "john@doe.com", 
+											role_id: 2, organization_id: 3
+										},
+										{ fname: "Ann", lname: "Smith", password: "asdf1234",
+											password_confirmation: 'asdf1234', email: "jane@smith.com",
+											role_id: 2, organization_id: 1
+										},
+										{ fname: "Student", lname: "Tester", password: "asdf1234",
+											password_confirmation: 'asdf1234', email: "student@ops.edu",
+											role_id: 2, organization_id: 2
+										}])
