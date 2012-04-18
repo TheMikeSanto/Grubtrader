8fdcd2f621e974129f9a1ba467971a9bf91bec63
Mike Santo <mike.j.santo@gmail.com>
2012-04-17 23:41:44 -0500

    redirect to new_user_session_path after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+  # Overwrite redirect path after signout
+  def after_sign_out_path_for(resource_or_scope)
+  	new_user_session_path
+  end
 end
