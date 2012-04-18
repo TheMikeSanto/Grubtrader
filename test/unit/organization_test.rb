4e3bec103b48cdf26fc9dede2f84dc7d23105aea
Mike Santo <mike.j.santo@gmail.com>
2012-04-18 01:06:27 -0500

    added some organization auth tests. they dont pass right now

--- test/unit/organization_test.rb
+++ test/unit/organization_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class OrganizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	setup do
+		@user1 = users(:user1)
+		@user2 = users(:user2)
+		@admin = users(:admin1)
+
+		@user1_ability = Ability.new(@user1)
+		@user2_ability = Ability.new(@user2)
+		@guest_ability = Ability.new(User.new)
+		@admin_ability = Ability.new(@admin)
+
+		@organization = organizations(:one)
+	end
+
+	test "should be creatable and readable by anyone" do
+		assert @guest_ability.can?(:read, @organization)
+		assert @guest_ability.can?(:create, Organization)
+	end
+
+	test "should only be updateable by someone in the organization" do
+		assert @user2_ability.can?(:update, @organization)
+	end
+
+	test "should not be updateable by someone not in the organization" do
+		assert @user1_ability.cannot?(:update, @organization)
+	end
+
+	test "should not be destroyable by non-admins" do
+		assert @user1_ability.cannot?(:destroy, @organization)
+		assert @user2_ability.cannot?(:destroy, @organization)
+	end
+
+	test "should be completely accessible to admins" do
+		assert @admin_ability.can?(:update, @organization)
+		assert @admin_ability.can?(:destroy, @organization)
+		assert @admin_ability.can?(:read, @organization)
+	end
 end
