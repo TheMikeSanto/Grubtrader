ec8c00fcc35dbab1182031e64e011d76b1eb253e
Mike Santo <mike.j.santo@gmail.com>
2012-04-19 18:56:11 -0500

    make any new organizations producers by default

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -24,7 +24,7 @@ class OrganizationsController < ApplicationController
   # GET /organizations/new
   # GET /organizations/new.json
   def new
-    @organization = Organization.new
+    @organization = Organization.new(role_id: OrganizationRole.producer_role_id)
 
     respond_to do |format|
       format.html # new.html.erb
--- app/models/organization_role.rb
+++ app/models/organization_role.rb
@@ -1,3 +1,11 @@
 class OrganizationRole < ActiveRecord::Base
 	has_many :organizations
+
+	def self.producer_role_id
+		OrganizationRole.find_by_name("Producer").id
+	end
+
+	def self.distributor_role_id
+		OrganizationRole.find_by_name("Distributor").id
+	end
 end
