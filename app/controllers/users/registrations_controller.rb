041ede4228e79283c88135c9c0623a191c3b4e61
Mike Santo <mike.j.santo@gmail.com>
2012-04-19 20:31:05 -0500

    user registrations controller takes params from other controllers, fills in org box after a user creates an org

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 	
+  def new
+  @user = User.new(params[:user])
+
+	  respond_to do |format|
+	    format.html # new.html.erb
+	    format.json { render json: @user }
+	  end
+  end
 end
