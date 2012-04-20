0918ce905f4d3f51a2e472c8e41b23e323ee31b1
Mike Santo <mike.j.santo@gmail.com>
2012-04-19 19:58:33 -0500

    no one can touch organizationroles now

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,12 +19,13 @@ class Ability
       can :manage, Report
       can :manage, Inventory
       can :view, Product
-      can [:update], Organization, organization_id: user.organization_id
+      can :update, Organization, organization_id: user.organization_id
     end
 
     if user.is_admin?
       can :manage, :all
       cannot [:update, :destroy, :create], Role
+      cannot [:update, :destroy, :create], OrganizationRole
     end
   end
 end
