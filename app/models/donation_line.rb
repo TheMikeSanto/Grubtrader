dc8635998fdc1e32beecbdf7c06f7639d6fbd2fa
Mike Santo <mike.j.santo@gmail.com>
2012-04-22 21:21:32 -0500

    hacking at getting the datetime formats to work

--- app/models/donation_line.rb
+++ app/models/donation_line.rb
@@ -1,4 +1,10 @@
 class DonationLine < ActiveRecord::Base
 	belongs_to :donation
 	belongs_to :product
+
+	before_save :convert_date_to_datetime
+
+	def convert_date_to_datetime
+		date = DateTime.new(date)
+	end
 end
