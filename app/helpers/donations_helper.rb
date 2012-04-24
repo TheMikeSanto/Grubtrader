d3a1b63438ee0d3b44fcc0dd629e3f100cc05c80
Mike Santo <mike.j.santo@gmail.com>
2012-04-24 18:38:45 -0500

    created helper method to display summary of products in donation

--- app/helpers/donations_helper.rb
+++ app/helpers/donations_helper.rb
@@ -1,2 +1,5 @@
 module DonationsHelper
+	def donation_product_summary(donation, product)
+		[donation.total_product_quantity(product).to_s, product.unit.name, product.name.downcase].join(" ")
+	end
 end
