b53c476aa1e550bc7cbd31b1cc3051e83fe77ec2
Mike Santo <mike.j.santo@gmail.com>
2012-04-24 18:18:04 -0500

    method on order line to return the matched donation line

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -6,4 +6,8 @@ class OrderLine < ActiveRecord::Base
 	def filled?
 		quantity_filled == quantity_requested
 	end
+
+	def donation_line
+		Inventory.find_by_order_line_id(id).donation_lines.first
+	end
 end
