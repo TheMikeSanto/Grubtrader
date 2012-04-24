c9194da8885274b89b4e6fcf48dc6863e213c467
Mike Santo <mike.j.santo@gmail.com>
2012-04-23 19:01:05 -0500

    made order lines dependent destroy to orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,7 @@
 class Order < ActiveRecord::Base
 	belongs_to :user
 	has_one :organization, through: :user
-	has_many :order_lines
+	has_many :order_lines, dependent: :destroy
 
 	accepts_nested_attributes_for :order_lines
-
 end
