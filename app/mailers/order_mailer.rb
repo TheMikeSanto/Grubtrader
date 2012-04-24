f36bcd38a5ec1c5850e875e1c18105b241dfdbe7
Mike Santo <mike.j.santo@gmail.com>
2012-04-24 15:56:19 -0500

    added a mailer to notify donors when their donations have been ordered for pickup

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,4 +6,13 @@ class OrderMailer < ActionMailer::Base
   	@order = order
   	mail(to: @user.email, subject: "Order confirmation: order ##{order.id}")
   end
+
+  def notify_donor(order, donation_line)
+  	@donation_line = donation_line
+  	@donation = donation_line.donation
+  	@donor = @donation.user
+  	@order = order
+  	mail(to: @donor.email, subject: "An item you donated needs pickup - Donation ##{@donation.id})")
+	end
+
 end
--- app/observers/order_observer.rb
+++ app/observers/order_observer.rb
@@ -2,5 +2,13 @@ class OrderObserver < ActiveRecord::Observer
 	def after_create(order)
 		order.create_inventory
 		OrderMailer.order_confirmation(order).deliver
+
+		# Notify donators
+		order.order_lines.each do |line|
+			donations = Inventory.find_by_order_line_id(line.id).donation_lines
+			donations.each do |donation|
+				OrderMailer.notify_donator(order, donation)
+			end
+		end
 	end
 end
