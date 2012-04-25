13d42b5b3045cacd204d986eca017e04a96e25c8
Mike Santo <mike.j.santo@gmail.com>
2012-04-24 21:21:32 -0500

    get rid of routes for line items

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,9 @@ Foodbank::Application.routes.draw do
   devise_for :users,  controllers:  {registrations: "users/registrations"},
                       path_names:   {sign_in: "login", sign_out: "logout"}
 
-  resources :donation_lines
-
   resources :donations
 
   resources :orders
-
-  resources :order_lines
 
   match "/inventory" => "inventory#index"
 
