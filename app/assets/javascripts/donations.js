cd94e38d2b2c39786f8c5afc4ba7a00437d47a5b
Mike Santo <mike.j.santo@gmail.com>
2012-05-03 17:23:43 -0500

    corrected unit display on new donation page

--- app/assets/javascripts/donations.js
+++ app/assets/javascripts/donations.js
@@ -3,4 +3,13 @@ $(function() {
 		{ dateFormat: "dd-mm-yy",
 			minDate: '-1m'
 	});
+
+	$("#new_donation select").change(function() {
+		selected = $(this).find("option:selected");
+		line_item = selected.closest(".line_item");
+		product = selected.html();
+		unit = $("#" + product).data("unit");
+
+		line_item.find(".unit").html(unit);
+	})
 });
