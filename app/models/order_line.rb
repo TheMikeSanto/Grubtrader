22a91b71328c4d551ea39c92fb6d07d167c17d26
Mike Santo <mike.j.santo@gmail.com>
2012-05-05 06:09:19 +0200

    better donation_lines method on order line

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -2,12 +2,13 @@ class OrderLine < ActiveRecord::Base
 	belongs_to :order
 	belongs_to :product
 	has_one :unit, through: :product
+	has_one :inventory
 
 	def filled?
 		quantity_filled == quantity_requested
 	end
 
 	def donation_line
-		Inventory.find_by_order_line_id(id).donation_lines.first
+		inventory.donation_lines.first
 	end
 end
