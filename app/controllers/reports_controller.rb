49c4591b50c2cd57d80bef4f05eb988e2d958154
Mike Santo <mike.j.santo@gmail.com>
2012-05-05 09:04:47 +0200

    simple reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,81 +2,20 @@ class ReportsController < ApplicationController
   # GET /reports
   # GET /reports.json
   def index
-    @reports = Report.all
+    @daily_orders = Order.where(created_at: Date.today).count
+    @daily_donations = Donation.where(created_at: Date.today).count
+    @weekly_orders = Order.where("created_at > ?", 1.week.ago).count
+    @weekly_donations = Donation.where("created_at > ?", 1.week.ago).count
+    @monthly_orders = Order.where("created_at > ?", 1.month.ago).count
+    @monthly_donations = Donation.where("created_at >?", 1.month.ago).count
+    @yearly_orders = Order.where("created_at > ?", 1.year.ago).count
+    @yearly_donations = Donation.where("created_at >?", 1.year.ago).count
+    @all_orders = Order.all.count
+    @all_donations = Donation.all.count
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @reports }
-    end
-  end
-
-  # GET /reports/1
-  # GET /reports/1.json
-  def show
-    @report = Report.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @report }
-    end
-  end
-
-  # GET /reports/new
-  # GET /reports/new.json
-  def new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @report }
-    end
-  end
-
-  # GET /reports/1/edit
-  def edit
-    @report = Report.find(params[:id])
-  end
-
-  # POST /reports
-  # POST /reports.json
-  def create
-    @report = Report.new(params[:report])
-
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to @report, notice: 'Report was successfully created.' }
-        format.json { render json: @report, status: :created, location: @report }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /reports/1
-  # PUT /reports/1.json
-  def update
-    @report = Report.find(params[:id])
-
-    respond_to do |format|
-      if @report.update_attributes(params[:report])
-        format.html { redirect_to @report, notice: 'Report was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /reports/1
-  # DELETE /reports/1.json
-  def destroy
-    @report = Report.find(params[:id])
-    @report.destroy
-
-    respond_to do |format|
-      format.html { redirect_to reports_url }
-      format.json { head :ok }
     end
   end
 end
